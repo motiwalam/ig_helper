497f06c25951b508ec1048aba57798672282910c
thechosenreader <mmotiwala53@gmail.com>
2021-12-09 17:53:51 -0500

    hijack __d() to find magic module id

--- ighelper.user.js
+++ ighelper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name IG Helper
-// @version 1.0.0
+// @version 1.1
 // @description Provides scripting functionality to instagram.com
 // @author chosen
 
@@ -8,20 +8,176 @@
 // @match https://www.instagram.com/*
 
 
+// @require https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @grant GM_getValue
+// @grant GM_setValue
+
+// @run-at document-start
+
 // ==/UserScript==
 
-// magic module that contains definition of sendTextMessage
-// is required in sdm
-const MM = window.__r(13435555);
+_window = unsafeWindow;
+// _window = window;
 
-// generate random string of digits
-grandom = n => Array(n).fill(null).map(c => Math.floor(Math.random()*10)).join("");
+// create scheduler config
+csc = () => {
+    const sid = "SchedulerConfig";
+    GM_config.init(
+        {
+            id: sid,
+            fields: {
+                m1: {
+                    section: [
+                        'Messages to be scheduled'
+                    ],
+                    label: "Message 1",
+                    type: "message",
 
-// send a direct message s to thread t
-sdm = (t, s) => MM.sendTextMessage(`${t}`, `${s}`, grandom(15));
+                }
+            },
 
-// get thread id from window location
-gtid = () => window.location.toString().split('/t/')[1];
+            types: {
+                message: {
+                    default: null,
+                    toNode: function(configId) {
+                        debugger;
+                        const field = this.settings,
+                              value = this.value,
+                              id = this.id,
+                              create = this.create,
+                              retNode = create('div', {
+                                  className: 'config_var',
+                                  id: `${configId}_${id}_var`,
+                                  title: field.title || ''
+                              });
 
-// schedule a message s in thread t to run in d milliseconds
-schedule = (d, t, s) => setTimeout(sdm, d, t, s);
+                        retNode.appendChild(create('label', {
+                            innerHTML: field.label,
+                            id: `${configId}_${id}_field_label`,
+                            for: `${configId}_field_${id}`,
+                            className: 'field_label',
+                        }));
+
+                        ['tid', 'delay', 'message'].forEach(i => {
+                            const props = {
+                                id: `${configId}_field_${id}_${i}`,
+                                type: 'text',
+                                size: '100',
+                                value: value ? value[i] : "",
+
+                            }
+                            retNode.appendChild(create('input', props));
+                        });
+
+                        return retNode;
+                    },
+
+                    toValue: () => {
+                        if (this.wrapper) {
+                            const id = this.id;
+                            const rval = {};
+                            ['tid', 'delay', 'message'].forEach(i => {
+                                var input = this.wrapper.getElementById(`${configId}_field_${id}_${i}`);
+                                rval[i] = (input.value);
+                            })
+
+                            return rval;
+                        }
+
+                        return null;
+                    },
+
+                    reset: () => {
+
+                    },
+                }
+            }
+        }
+    );
+
+    GM_config.open();
+}
+
+hijack = () => {
+    /*
+        this code is running at document-start, ideally before
+        *any* of instagram's code is run.
+
+        before any javascript files are loaded, a <script> tag is
+        executed that defines logic for requiring and defining
+        modules.
+
+        the end result is that the window object contains two functions:
+            __d(f, i, d) defines a module with a factory function f, an id i, and dependencies d
+            __r(i) requires (and initializes, if necessary) the module with id i
+
+        since these ids are susceptible to change, it is not feasible to hard code
+        the id of the module that implements direct message functionality.
+        the problem gets worse if we want functionality from yet other modules.
+
+        the id's are also not named in a consistent, predictable way.
+
+        to get access to all of them, we define __d *before* instagram defines it
+        with a custom setter and getter.
+
+        the set function should be used only once, when instagram defines it, and it
+        serves the purpose of saving the correct functionality of __d under a different name
+
+        the get function then returns a wrapper around the "real" __d that simply passes
+        its arguments through unchanged, but stores the id used to define the module before
+        it does so.
+
+        at the end, all the modules are defined, and the ids of all of them are accessible
+        in the all_ids object.
+
+    */
+    _window.__all_ids = [];
+    _window.__all_calls = [];
+    Object.defineProperty(_window, '__d', {
+        set: f => _window.__dd = f,
+        get: () => (...args) => {
+            _window.__all_ids.push(args[1]);
+            _window.__all_calls.push(args);
+            return _window.__dd(...args);
+        }
+    });
+}
+
+init = () => {
+    // everything below runs after
+    // DOMContentLoaded
+
+    // --------- HELPER FUNCTIONS ---------
+
+    // find the ids of modules matching the key tp
+    getMMId = (tp) => {
+        // refer to hijack() to see where __all_ids comes from
+        return _window.__all_ids.filter(i => {
+            var m = _window.__r(i);
+            return m && (m == tp || m.hasOwnProperty(tp));
+        })
+    }
+
+    // generate random string of digits
+    grandom = n => Array(n).fill(null).map(c => Math.floor(Math.random()*10)).join("");
+
+    // send a direct message s to thread t
+    sdm = (t, s) => DM_module.sendTextMessage(`${t}`, `${s}`, grandom(15));
+
+    // get thread id from window location
+    gtid = () => _window.location.toString().split('/t/')[1];
+
+    // schedule a message s in thread t to run in d milliseconds
+    schedule = (d, t, s) => setTimeout(sdm, d, t, s);
+
+    // --------- MODULE DEFINITIONS ---------
+
+    // used for
+    //  * sendTextMessage()
+    const DM_module = _window.__r(getMMId("sendTextMessage")[0]);
+
+    console.log("IG Helper running");
+}
+
+hijack();
+document.addEventListener("DOMContentLoaded", init);
